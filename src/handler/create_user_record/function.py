c2ea3e248f6ac1b9798ed16db53e197a36aa35cc
Yoojin93 <yoojinbae93@gmail.com>
2024-01-06 16:11:41 -0600

    fixed item_data

--- src/handler/create_user_record/function.py
+++ src/handler/create_user_record/function.py
@@ -12,21 +12,8 @@ def handler(event: APIGatewayProxyEvent, context):
     logging.info('Creating User Record', extra={"message_object": event})
     try:
         payload = event['body']
-        
-        item_data = {
-            'PK': payload['PK']['S'],
-            'SK': payload['SK']['S'],
-            'GSI_PK': payload['GSI_PK']['S'],
-            'GSI_SK': payload['GSI_SK']['S'],
-            'user_data': {
-                'user_id': payload['user_data']['M']['user_id']['S'],
-                'name': payload['user_data']['M']['name']['S'],
-                'email': payload['user_data']['M']['email']['S'],
-                'creation_date': payload['user_data']['M']['creation_date']['S']
-            }
-        }
 
-        ddbtable._put_item(Item=item_data)
+        ddbtable._put_item(Item=payload)
 
         return {
             'statusCode': 200,
